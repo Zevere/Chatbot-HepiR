9d8a0e985595451dc54ddfbeb697c2f29bdcd5ba
Kevin Ma <kbma.kevin@gmail.com>
2018-12-09 16:17:46 -0500

    Added remaining tests for core functionality TDD

--- tests/test_helper_methods.py
+++ tests/test_helper_methods.py
@@ -2,7 +2,62 @@ from helper_methods import *
 from test_properties import *
 
 
+class TestAuthentication(object):
+    def test_login(self):
+        # TEST ENV SETUP
+        # make sure the valid account is logged out first before trying to login with the account
+        print('*'*50)
+        print('TEST ENVIRONMENT SETUP')
+        print(disconnect(*VALID_ZV_CONNECTION)[1])
+        print('*'*50)
+
+        # trying to login with valid credentials
+        assert connect(*VALID_ZV_CONNECTION) == (
+            True, 'You have successfully logged into the Zevere account {} with your telegram account of {}'.format(VALID_ZV_CONNECTION[0], VALID_ZV_CONNECTION[1]))
+
+        # trying to login with invalid credentials
+        assert connect(*INVALID_ZV_CONNECTION) == (
+            False, 'You have provided invalid login credentials.')
+
+    # def test_is_authenticated(self):
+    # TEST ENV SETUP
+    # make sure the valid account is logged in first before trying to logout with the account
+    # connect(*VALID_ZV_CONNECTION)
+    #     # test authentication for valid tg->zv connection
+    #     # should return True and the zv_user the tg user is auth'd as
+    #     assert is_authenticated(
+    #         *VALID_ZV_CONNECTION) == (True, VALID_ZV_CONNECTION[0])
+
+    #     # test authentication for invalid tg->zv connection
+    #     # should return False and None since tg user not auth'd
+    #     assert is_authenticated(*INVALID_ZV_CONNECTION) == (False, None)
+
+    def test_logout(self):
+
+        # TEST ENV SETUP
+        # make sure the valid account is logged in first before trying to logout with the account
+        print('*'*50)
+        print('TEST ENVIRONMENT SETUP')
+        print(connect(*VALID_ZV_CONNECTION)[1])
+        print('*'*50)
+
+        # trying to logout valid telegram to zevere connection
+        assert disconnect(*VALID_ZV_CONNECTION) == (
+            True, 'You have successfully been logged out from {}'.format(VALID_ZV_CONNECTION[0]))
+
+        # trying to logout with invalid telegram to zevere connection
+        assert disconnect(*INVALID_ZV_CONNECTION) == (
+            False, 'You are not logged in to any Zevere account.')
+
+
 class TestListManagmement(object):
+     # TEST ENV SETUP
+    # make sure the valid account is logged in first before trying to logout with the account
+    print('*'*50)
+    print('TEST ENVIRONMENT SETUP')
+    print(connect(*VALID_ZV_CONNECTION)[1])
+    print('*'*50)
+
     def test_get_all_lists(self):
         # returns list of lists for existant zevere user
         assert type(get_all_lists(VALID_ZEVERE_USER)) == list
@@ -48,41 +103,81 @@ class TestListManagmement(object):
     #     assert get_list_by_id(INVALID_LIST_ID) is None
 
 
-class TestAuthentication(object):
-    def test_login(self):
-        # TEST ENV SETUP
-        # make sure the valid account is logged out first before trying to login with the account
-        disconnect(*VALID_ZV_CONNECTION)
+# class TestTaskManagement(object):
+#     # TEST ENV SETUP
+#     # make sure the valid account is logged in first before trying to logout with the account
+#     print('*'*50)
+#     print('TEST ENVIRONMENT SETUP')
+#     print(connect(*VALID_ZV_CONNECTION)[1])
+#     print('The list with id = {} was successfully created'.format(
+#         create_list(VALID_ZEVERE_USER, LIST_TITLE, LIST_DESCRIPTION)))
+#     print('*'*50)
 
-        # trying to login with valid credentials
-        assert connect(*VALID_ZV_CONNECTION) == (
-            True, 'You have successfully logged into the Zevere account {} with your telegram account of {}'.format(VALID_ZV_CONNECTION[0], VALID_ZV_CONNECTION[1]))
+#     def test_get_all_tasks(self):
+#         # returns list of tasks for existant zevere user and his/her owned list
+#         assert type(get_all_tasks(VALID_ZEVERE_USER, VALID_LIST_ID)) == list
 
-        # trying to login with invalid credentials
-        assert connect(*INVALID_ZV_CONNECTION) == (
-            False, 'You have provided invalid login credentials.')
-        pass
+#         # should return null for existant zevere user but an unknown list
+#         assert get_all_tasks(VALID_ZEVERE_USER, INVALID_LIST_ID) is None
 
-    # def test_is_authenticated(self):
-    #     # test authentication for valid tg->zv connection
-    #     # should return True and the zv_user the tg user is auth'd as
-    #     assert is_authenticated(
-    #         *VALID_ZV_CONNECTION) == (True, VALID_ZV_CONNECTION[0])
+#         # should return null for non existant zevere users
+#         assert get_all_tasks(INVALID_ZEVERE_USER, VALID_LIST_ID) is None
 
-    #     # test authentication for invalid tg->zv connection
-    #     # should return False and None since tg user not auth'd
-    #     assert is_authenticated(*INVALID_ZV_CONNECTION) == (False, None)
+#     def test_create_task(self):
+#         # create new task for the zevere user for an existing list
+#         # should return the id of the task which is the task title
+#         # toLower and spaces replaced with underscore characters
+#         assert create_task(VALID_ZEVERE_USER, VALID_LIST_ID, TASK_TITLE,
+#                            TASK_DESCRIPTION) == (True, TASK_TITLE.strip().lower().replace(' ', '_'))
 
-    def test_logout(self):
+#         # creating a task that already exists for the zevere user's list
+#         # should return None since the task is not created again
+#         assert create_task(VALID_ZEVERE_USER, VALID_LIST_ID,
+#                            TASK_TITLE, TASK_DESCRIPTION) == (False, None)
 
-        # TEST ENV SETUP
-        # make sure the valid account is logged in first before trying to logout with the account
-        connect(*VALID_ZV_CONNECTION)
+#         # creating a task for a non-existent list
+#         # should return None since the task cannot be created
+#         assert create_task(VALID_ZEVERE_USER, INVALID_LIST_ID,
+#                            TASK_TITLE, TASK_DESCRIPTION) == (False, None)
 
-        # trying to logout valid telegram to zevere connection
-        assert disconnect(*VALID_ZV_CONNECTION) == (
-            True, 'You have successfully been logged out from {}'.format(VALID_ZV_CONNECTION[0]))
+#         # creating a task for a list not belonging to the connected zevere user
+#         # should return None since the task will not be created
+#         assert create_task(VALID_ZEVERE_USER, VALID_LIST_ID2,
+#                            TASK_TITLE, TASK_DESCRIPTION) == (False, None)
 
-        # trying to logout with invalid telegram to zevere connection
-        assert disconnect(*INVALID_ZV_CONNECTION) == (
-            False, 'You are not logged in to any Zevere account.')
+#         # creating a list with an invalid zevere user
+#         # should return None since the list is not created
+#         assert create_task(INVALID_ZEVERE_USER, VALID_LIST_ID,
+#                            LIST_TITLE, LIST_DESCRIPTION) == (False, None)
+
+#     def test_delete_task(self):
+#         # trying to delete a task not part of the list passed
+#         # in as an argument should return False and None as no
+#         # task was deleted
+#         assert delete_task(*VALID_LIST_AND_OWNER, VALID_TASK_ID2) == (
+#             False, None)
+
+#         # trying to delete a task by an id that does not exist
+#         # should return False and None as no list was deleted
+#         assert delete_list(*INVALID_OWNER_AND_LIST, INVALID_TASK_ID) == (
+#             False, None)
+
+#         # trying to delete a task part of the list owned by th zv_user passed
+#         # in as an argument should return True and the id of the list that was deleted
+#         assert delete_list(*VALID_LIST_AND_OWNER, VALID_TASK_ID) == (
+#             True, VALID_TASK_ID)
+
+#     def test_get_task_by_id(self):
+#         # try to get the task we created in the above test case
+#         result = get_task_by_id(VALID_TASK_ID)
+#         assert hasattr(result, 'id') & result['id'] == VALID_TASK_ID
+
+#         # try to get a non existent task
+#         assert get_task_by_id(INVALID_TASK_ID) is None
+
+#     print('*'*50)
+#     print('TEST ENVIRONMENT CLEANUP')
+#     print('The list with id = {} was successfully deleted'.format(
+#         delete_list(*VALID_LIST_AND_OWNER)[1]))
+#     print(disconnect(*VALID_ZV_CONNECTION)[1])
+#     print('*'*50)
--- tests/test_properties.py
+++ tests/test_properties.py
@@ -8,11 +8,21 @@ VALID_ZV_CONNECTION = VALID_ZEVERE_USER, VALID_TG_ID
 INVALID_ZV_CONNECTION = INVALID_ZEVERE_USER, INVALID_TG_ID
 
 LIST_TITLE = "Centennial College School Work"
+LIST_TITLE2 = "Test List with No Description"
 LIST_DESCRIPTION = "This task list is for managing all the school work I have to do from class."
 
 VALID_LIST_ID = LIST_TITLE.strip().lower().replace(' ', '_')
+VALID_LIST_ID2 = LIST_TITLE2.strip().lower().replace(' ', '_')
 INVALID_LIST_ID = 'xxx. chalbye choo foo?'
 
 VALID_LIST_AND_OWNER = VALID_ZEVERE_USER, VALID_LIST_ID
 INVALID_OWNER_VALID_LIST = INVALID_ZEVERE_USER, VALID_LIST_ID
-INVALID_OWNER_AND_LIST = VALID_ZEVERE_USER, INVALID_LIST_ID
+INVALID_OWNER_AND_LIST = INVALID_ZEVERE_USER, INVALID_LIST_ID
+
+TASK_TITLE = 'Rumi Essay'
+TASK_TITLE2 = 'Test Task with No Description'
+TASK_DESCRIPTION = 'Write 100 page research essay on the philosophical teachings of Rumi'
+
+VALID_TASK_ID = TASK_TITLE.strip().lower().replace(' ', '_')
+VALID_TASK_ID2 = TASK_TITLE2.strip().lower().replace(' ', '_')
+INVALID_TASK_ID = 'monsTer W3nk4r?? W0t 1s th|s?'
